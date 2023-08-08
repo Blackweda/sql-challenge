03b05d94f81fea8f37d1061cf7d610ae814e8731
Wayne Williams <siloaman@hotmail.ca>
2023-08-08 14:31:30 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,11 +1,18 @@
-CREATE TABLE department (
+CREATE TABLE Department (
     dept_no varchar not null,
     dept_name varchar not null,
     PRIMARY KEY (dept_no)
 );
 
 
-CREATE TABLE employee (
+CREATE TABLE Title (
+    title_id varchar not null,
+    title varchar not null,
+    PRIMARY KEY (title_id)
+);
+
+
+CREATE TABLE Employee (
     emp_no int not null,
     emp_title_id varchar not null,
     birth_date date not null,
@@ -13,41 +20,32 @@ CREATE TABLE employee (
     last_name varchar not null,
     sex varchar not null,
     hire_date date not null,
-    FOREIGN KEY (emp_title_id) REFERENCES title (title_id),
+    FOREIGN KEY (emp_title_id) REFERENCES Title (title_id),
     PRIMARY KEY (emp_no)
 );
 
 
-CREATE TABLE salary (
+CREATE TABLE Salary (
     emp_no int not null,
     salary int not null,
-    FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES Employee (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE title (
-    title_id varchar not null,
-    title varchar not null,
-    PRIMARY KEY (title_id)
-);
 
-
-
-
-CREATE TABLE dept_manager (
-    dept_no varchar not null,
-    emp_no int not null,
-    FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES department (dept_no),
-    PRIMARY KEY (dept_no, emp_no)
-);
-
-CREATE TABLE dept_emp (
+CREATE TABLE Dep_Emp_Junction (
     emp_no int not null,
     dept_no varchar not null,
-    FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES department (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employee (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Department (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 
+CREATE TABLE Dep_Manager_Junction (
+    dept_no varchar not null,
+    emp_no int not null,
+    FOREIGN KEY (emp_no) REFERENCES Employee (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Department (dept_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
